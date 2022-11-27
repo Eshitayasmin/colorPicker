061ac33c509dd58b053fba26d80c8bfe096b77da
Eshitayasmin <eshita.emc2k17@gmail.com>
2022-11-27 17:21:34 +0600

    color change state add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ function App() {
   const [color, setColor] = useState("");
   return (
     <div className="App">
-  
+      <div>
+      <form action="">
+      <textarea style={{background: color, height: '25vh'}} name="" id="" cols="40" rows="10"></textarea>
+      <br />
+      <input type="text" onChange={(e) => setColor(e.target.value)}/>
+     </form>
+      </div>
     </div>
   );
 }
