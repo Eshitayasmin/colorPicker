6b3d56761bed9e9f63c7ae54039ca832207584e6
Eshitayasmin <eshita.emc2k17@gmail.com>
2022-11-28 11:23:05 +0600

    readme file update

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 
 function App() {
   const [color, setColor] = useState("");
+  
   return (
     <div className="App">
-      <div>
-        <h1><img src="https://colorhunt.co/img/color-hunt-logo.gif" alt="" /> COLOR PICKER</h1>
-      <form action="">
-      <textarea className='common' style={{background: color}} name="" id="" cols="40" rows="10" readOnly></textarea>
-      <br />
-      <input className='common' type="text" onChange={(e) => setColor(e.target.value)} placeholder="Enter color name/code"/>
-     </form>
-      </div>
+        <h1>
+          <img src="https://colorhunt.co/img/color-hunt-logo.gif" alt=""/>
+         COLOR PICKER
+        </h1>
+
+        <form>
+          <textarea className='common' style={{ background: color }} name="text-area" cols="40" rows="10" readOnly></textarea>
+
+          <br />
+
+          <input className='common' type="text" onChange={(e) => setColor(e.target.value)} placeholder="Enter color name/code" />
+        </form>
     </div>
   );
 }
